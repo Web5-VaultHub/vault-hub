77075d9c3e6bf36a6c7baff888ca6aa6a142385a
ISIAKA ABDULAHI <56041462+isiakaabd@users.noreply.github.com>
2023-12-05 09:28:14 +0100

    resolved conflict from dev branch

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,5 +1,13 @@
 import Wrapper from "src/components/Wrapper";
 
 export default function Profile() {
-  return <Wrapper pageTitle="Wrapper"></Wrapper>;
+  return (
+    <Wrapper pageTitle="Wrapper">
+      <div>
+        <h3 className="text-2xl text-center font-bold">
+          Just testing Tailwind
+        </h3>
+      </div>
+    </Wrapper>
+  );
 }
